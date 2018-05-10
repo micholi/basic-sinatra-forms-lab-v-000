9da71605ee0fe1eff5b39b21990a6fcf906990a1
Michelle Olivieri <michelleo72@gmail.com>
2018-05-10 20:47:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,23 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @team = params[:team]
+    @team = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    
+    <p>Team Name: <input type="text" name="name"></p>
+    <p>Coach: <input type="text" name="coach"></p>
+    <p>Point Guard: <input type="text" name="pg"></p>
+    <p>Shooting Guard: <input type="text" name="sg"></p>
+    <p>Small Forward: <input type="text" name="sf"></p>
+    <p>Power Forward: <input type="text" name="pf"></p>
+    <p>Center: <input type="text" name="c"></p>
+
+    
     erb :team
   end
 end
