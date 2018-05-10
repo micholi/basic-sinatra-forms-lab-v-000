638ba8ee57dbd6924b7a32ad9a529b51ec39d892
Michelle Olivieri <michelleo72@gmail.com>
2018-05-10 20:47:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,16 +14,7 @@ class App < Sinatra::Base
     @sf = params[:sf]
     @pf = params[:pf]
     @c = params[:c]
-    
-    <p>Team Name: <input type="text" name="name"></p>
-    <p>Coach: <input type="text" name="coach"></p>
-    <p>Point Guard: <input type="text" name="pg"></p>
-    <p>Shooting Guard: <input type="text" name="sg"></p>
-    <p>Small Forward: <input type="text" name="sf"></p>
-    <p>Power Forward: <input type="text" name="pf"></p>
-    <p>Center: <input type="text" name="c"></p>
 
-    
     erb :team
   end
 end
